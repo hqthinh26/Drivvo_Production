edb45a3d98b3c62d58c5829b1d0b0767af9f0daf
quocthinh2609 <65326420+quocthinh2609@users.noreply.github.com>
2020-05-17 12:22:15 +0700

    rename a few columns

--- tpl-server-test/server/migrations/20200515_napnhienlieu.js
+++ tpl-server-test/server/migrations/20200515_napnhienlieu.js
@@ -11,17 +11,17 @@ module.exports = {
         create table napnhienlieu(
             id uuid PRIMARY KEY default uuid_generate_v4(),
             filling_date date default now(),
-            filling_hour time default now(),
-            odometer decimal(6,1) NOT NULL,
-            type_of_fuel text,
-            price_per_unit int,
-            total_cost int,
-            unit_amount decimal(5,2),
+            filling_hour timetz default now(),
+            odometer decimal(7,1) NOT NULL,
+            type_of_fuel text NOT NULL,
+            price_per_unit int NOT NULL,
+            total_cost int NOT NULL,
+            total_units decimal(5,2),
             full_tank bool,
-            destination text
+            location text
         );
         
-        insert into napnhienlieu(odometer, type_of_fuel, price_per_unit, total_cost, unit_amount, full_tank, destination)
+        insert into napnhienlieu(odometer, type_of_fuel, price_per_unit, total_cost, total_units, full_tank, location)
         values (123.4,'gasoline',20000, 50000, 2.5, true, 'dongnai');
 
         ` ,{raw: true});
