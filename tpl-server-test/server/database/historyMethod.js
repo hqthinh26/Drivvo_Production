acb0db02a5d0163ef9bb7593f9dafb1200f4302e
quocthinh2609 <65326420+quocthinh2609@users.noreply.github.com>
2020-07-01 16:35:59 +0700

    use foreign key

--- tpl-server-test/server/database/historyMethod.js
+++ tpl-server-test/server/database/historyMethod.js
@@ -12,7 +12,7 @@ module.exports = {
             console.table(results.rows);
             return results.rows;
         } catch (err) {
-            throw new Error('Failed at return_all_form');
+            throw new Error({message:'Failed at return_all_form',ERR: err});
         }
     },
 
@@ -20,23 +20,53 @@ module.exports = {
         const {type_of_form, id_private_form} = each_history_form;
         if (type_of_form === 'napnhienlieu') {
             console.log('this is nll');
-            return pool.query(`select * from napnhienlieu where id = $1`, [id_private_form]);
+            return pool.query(`
+            SELECT nll.id, nll.odometer, lnl.name as type_of_fuel, nll.price_per_unit, nll.total_cost, nll.total_units, nll.full_tank, tx.name as gas_station, ld.name as reason, nll.time, nll.date 
+            FROM napnhienlieu as nll, loainhienlieu as lnl, tramxang as tx, lydo as ld
+            WHERE       (nll.id = $1)
+                    AND (nll.type_of_fuel = lnl.id)
+                    AND (nll.gas_station = tx.id)
+                    AND (nll.reason = ld.id)
+            `, [id_private_form]);
         }
         if (type_of_form === 'chiphi') {
             console.log('this is chiphi');
-            return pool.query(`select * from chiphi where id = $1`, [id_private_form]);
+            return pool.query(`
+            SELECT cp.id, cp.odometer, lcp.name as type_of_expense, cp.amount, dd.name as place, ld.name as reason, cp.note, cp.time, cp.date
+            FROM chiphi as cp, loaichiphi as lcp, diadiem as dd, lydo as ld
+            WHERE       (cp.id = $1)
+                    AND (cp.type_of_expense = lcp.id)
+                    AND (cp.place = dd.id)
+                    AND (cp.reason = ld.id)
+            `, [id_private_form]);
         }
         if (type_of_form === 'dichvu') {
             console.log('this is dichvu');
-            return pool.query(`select * from dichvu where id = $1`, [id_private_form]);
+            return pool.query(`
+            SELECT dv.id, dv.odometer, ldv.name as type_of_service, dv.amount, dd.name as place, dv.note, dv.time, dv.date
+            FROM dichvu as dv, loaidichvu as ldv, diadiem as dd
+            WHERE       (dv.id = $1)
+                    AND (dv.type_of_service = ldv.id)
+                    AND (dv.place = dd.id)
+            `, [id_private_form]);
         }
         if(type_of_form === 'thunhap') {
             console.log({message: 'this is thunhap',id_private_form});
-            return pool.query(`select * from thunhap where id = $1`, [id_private_form]);
+            return pool.query(`
+            SELECT tn.id, tn.odometer, ltn.name as type_of_income, tn.amount, tn.note, tn.time, tn.date
+            FROM thunhap as tn, loaithunhap as ltn
+            WHERE       (tn.id = $1)
+                    AND (tn.type_of_income = ltn.id) 
+            `, [id_private_form]);
         }
         if(type_of_form === 'quangduong') {
             console.log({message: 'this is quangduong',id_private_form});
-            return pool.query(`select * from quangduong where id = $1`, [id_private_form]);
+            return pool.query(`
+            SELECT qd.id, qd.origin, qd.start_time, qd.start_date, qd.initial_odometer, qd.destination, qd.end_time, qd.end_date, qd.final_odometer, qd.value_per_km, qd.total, ld.name as reason
+            FROM quangduong as qd, lydo as ld
+            WHERE       (qd.id = $1)
+                    AND (qd.reason = ld.id)
+            `, [id_private_form]);
         }
         if (type_of_form === 'nhacnho') {
             console.log('this is nhac nho');
